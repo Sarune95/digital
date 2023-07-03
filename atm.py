5ea316e796ea1f9e97da413a31d3ca0db70c8195
Sarune Matiukaite <sharonhews@yahoo.com>
2023-07-03 17:20:18 +0100

    Feat: Loading screen

--- atm.py
+++ atm.py
@@ -1,7 +1,27 @@
 """
 Digital bank virtual atm machine
 """
+import os
+import time
+from rich import print
 
+
+def loader():
+    """INTRO"""
+    numbers = ['5%', '10%', '15%', '20%', '25%', '30%', '35%',
+               '40%', '45%', '50%', '55%', '60%', '65%', '70%', 
+               '75%', '80%', '85%', '90%', '95%', '100%']
+    for number in numbers:
+        os.system('cls||clear')
+        print()
+        print("[green]==================================================[/]")
+        print()
+        print(f'               [blue]LOADING {number}[/]')
+        print()
+        print("[green]==================================================[/]")
+        time.sleep(0.1)
+
+loader()
 def atm():
     """Verification"""
     pin = "1234"
